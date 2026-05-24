82381bf15e32145930598bc260593ee5efa69f0a
Loo Khai Yann <kloo0028@student.monash.edu>
2026-05-24 17:39:47 +0800

    edited charts

---
+++ bump chart/js/bumpchart.js
@@ -0,0 +1,4 @@
+var vg_7 = "bump chart/js/bumpchart.json";
+vegaEmbed("#bump_chart_1", vg_7).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
