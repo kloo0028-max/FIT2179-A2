556562a5157e764322e17461ca695087f72efd16
Loo Khai Yann <kloo0028@student.monash.edu>
2026-05-30 20:55:15 +0800

    updated

--- Distribution of Monthly Donations (2020–2025)/js/boxplot.js
+++ Distribution of Monthly Donations (2020–2025)/js/boxplot.js
@@ -1,4 +1,4 @@
 var vg_8 = "Distribution of Monthly Donations (2020–2025)/js/boxplot.json";
-vegaEmbed("#boxplot_1", vg_8, {actions: false}).then(function(result) {
+vegaEmbed("#boxplot_1", vg_8, {actions: false}, {renderer: 'svg'}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
